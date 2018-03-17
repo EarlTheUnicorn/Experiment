02ca8fc816c4714eeb3fca6dae479730b5824d7c
DolmenofGods <37228220+DolmenofGods@users.noreply.github.com>
2018-03-17 12:18:54 -0400

    Add files via upload

---
+++ DestroyByContact.cs
@@ -0,0 +1,69 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DestroyByContact : MonoBehaviour {
+
+
+	public GameObject boom;
+	public GameObject boom2;
+	public GameObject boom3; 
+	public int scoreValue;
+	private Controller controller;
+
+
+	void Start(){
+		GameObject gameControllerObject = GameObject.FindWithTag ("GameController");
+		if (gameControllerObject != null)
+		{
+			controller = gameControllerObject.GetComponent <Controller>();
+		}
+		if (controller == null)
+		{
+			Debug.Log ("Cannot find 'GameController' script");
+		}
+	}
+	IEnumerator destroy() {
+		
+		GetComponent<MeshRenderer>().enabled = false;
+
+		Instantiate (boom, transform.position, transform.rotation);
+		yield return new WaitForSeconds(.15f);
+		Instantiate(boom2, transform.position, transform.rotation);
+		yield return new WaitForSeconds(.15f);
+		Instantiate(boom3, transform.position, transform.rotation);
+		yield return new WaitForSeconds(.15f);
+		Object.Destroy(GameObject.Find("Explosion_Effect_0.0.1_Stage_01(Clone)")); 
+		Object.Destroy(GameObject.Find("Explosion_Effect_0.0.1_Stage_02(Clone)")); 
+		Object.Destroy(GameObject.Find("Explosion_Effect_0.0.1_Stage_03(Clone)")); 
+		Object.Destroy (gameObject);
+
+
+	}
+		
+
+
+
+	void OnTriggerEnter(Collider other){
+		int exp = 0;
+		int lol = 0;
+		Debug.Log (other.name);
+		if (other.tag == "Boundary") {
+			return;
+		} 
+		if (other.tag == "Player") {
+			exp++;
+			lol++;
+			controller.GameOver ();
+
+		}
+		controller.UpdateScore (scoreValue);
+		if (exp == 0){
+			StartCoroutine (destroy ());
+
+		}
+		if (lol == 0) {
+			Destroy (other.gameObject);
+		}
+}
+}
---
+++ PlayerController.cs
@@ -0,0 +1,94 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+
+[System.Serializable]
+public class Boundary
+
+
+{
+	public float xMin, xMax, zMin, zMax;
+}
+
+public class PlayerController : MonoBehaviour
+{
+	private Rigidbody rb;
+
+	public float speed;
+	public float tilt;
+	public Boundary boundary;
+
+	private float nextFire = 0.5F;
+	public Transform ShotSpawn;
+	public GameObject shot;
+	public float fireRate = 0.5F;
+	public GameObject playerBoom;
+	public GameObject playerBoom2;
+	public GameObject playerBoom3;
+	public Transform tr;
+
+	void Update() {
+
+		if (Input.GetButton("Fire1") && Time.time > nextFire)
+		{
+			nextFire = Time.time + fireRate;
+			Instantiate(shot, ShotSpawn.transform.position, ShotSpawn.transform.rotation);
+		}
+
+	}
+	IEnumerator destroyPlayer() {
+		//yield return new WaitForSeconds(.f);
+
+		GetComponent<MeshRenderer>().enabled = false;
+		tr = GetComponent<Transform> ();
+		yield return new WaitForSeconds(.3f);
+		Instantiate (playerBoom2, tr.transform.position, Quaternion.Euler (-90.0f, 0.0f, 0));
+		yield return new WaitForSeconds(.3f);
+		Instantiate (playerBoom3, tr.transform.position, Quaternion.Euler (-90.0f, 0.0f, 0));
+		yield return new WaitForSeconds(.3f);
+		Instantiate (playerBoom, tr.transform.position, Quaternion.Euler (-90.0f, 0.0f, 0));
+		yield return new WaitForSeconds(.3f);
+		Object.Destroy(gameObject);
+		Object.Destroy(GameObject.Find("Player_Explosion_0.0.0_Stage_02(Clone)")); 
+		Object.Destroy(GameObject.Find("Player_Explosion_0.0.0_Stage_03(Clone)")); 
+		Object.Destroy(GameObject.Find("Player_Explosion_0.0.0_Stage_01(Clone)")); 
+
+	}
+
+	void FixedUpdate ()
+	{     
+		rb = GetComponent<Rigidbody> ();
+		//for(int x; x<1000; x+28){
+		//	}
+	
+		float moveHorizontal = Input.GetAxis ("Horizontal");
+		float moveVertical = Input.GetAxis ("Vertical");
+
+		Vector3 movement = new Vector3 (moveHorizontal, 0.0f, moveVertical);
+		rb.velocity = movement * speed;
+
+		rb.position = new Vector3 
+			(
+				Mathf.Clamp (rb.position.x, boundary.xMin, boundary.xMax), 
+				0.0f, 
+				Mathf.Clamp (rb.position.z, boundary.zMin, boundary.zMax)
+			);
+
+		rb.rotation = Quaternion.Euler (0.0f, 0.0f, rb.velocity.x * -tilt);
+	}
+
+
+	void OnTriggerEnter(Collider other){
+
+		//Debug.Log (other.name);
+
+		if (other.tag == "Boundary") {
+			return;
+		} 
+
+		StartCoroutine (destroyPlayer ());
+		Destroy(other.gameObject);
+
+	}
+
+}
